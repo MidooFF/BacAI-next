17123eadc8e65bf147510f562b2c1cb8a8f6a53a
tarek shno <mohammad.sh12121@gmail.com>
2026-01-24 21:46:30 +0300

    fixing netlify errors

---
+++ src/app/sitemap.js
@@ -0,0 +1,9 @@
+export default async function sitemap() {
+  return [
+    {
+      url: "https://bacai-next.netlify.app",
+      lastModified: new Date(),
+    },
+    // Add all your pages
+  ];
+}
